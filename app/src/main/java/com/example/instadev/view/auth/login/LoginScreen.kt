2194cb06a8064eb4fe3757fa16ff969651c3db07
rrufino71@gmail.com <rrufino71@gmail.com>
2026-05-22 09:41:41 -0300

    strings

--- app/src/main/java/com/example/instadev/view/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/instadev/view/auth/login/LoginScreen.kt
@@ -12,24 +12,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -58,7 +52,7 @@ fun LoginScreen(loginViewModel: LoginViewModel = viewModel()) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             InstaText(
-                text = "Español (España)",
+                text = stringResource(R.string.login_screen_header_text_spain),
                 modifier = Modifier.padding(top = 22.dp),
                 //style = MaterialTheme.typography.bodySmall
             )
@@ -75,7 +69,7 @@ fun LoginScreen(loginViewModel: LoginViewModel = viewModel()) {
                 value = uiState.email,
                 label = {
                     InstaText(
-                        text = "Usuario, correo electronico o movil",
+                        text = stringResource(R.string.login_screen_textfield_email),
                     )
                 },
                 onValueChange = { loginViewModel.onEmailChange(it) })
@@ -86,20 +80,20 @@ fun LoginScreen(loginViewModel: LoginViewModel = viewModel()) {
                 value = uiState.password,
                 label = {
                     InstaText(
-                        text = "Contraseña",
+                        text = stringResource(R.string.login_screen_textfield_password),
                     )
                 },
                 onValueChange = { loginViewModel.onPassword(it) })
             Spacer(Modifier.height(10.dp))
             InstaButton(
                 modifier = Modifier.fillMaxWidth(),
-                text = "Inicial Sesión",
+                text = stringResource(R.string.login_screen_button_login),
                 onClick = {},
                 enabled = uiState.isLoginEnabled,
             )
             TextButton(onClick = {}) {
                 InstaText(
-                    text = "Has olvidado tu contraseña ?",
+                    text = stringResource(R.string.login_screen_text_forgot_password),
                     color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
             }
@@ -109,7 +103,7 @@ fun LoginScreen(loginViewModel: LoginViewModel = viewModel()) {
                 onClick = {},
                 border = BorderStroke(width = 1.dp, MaterialTheme.colorScheme.primary)
                 //colors = ButtonDefaults.outlinedButtonColors(MaterialTheme.colorScheme.primary)
-            ) { InstaText(text = "Crear cuenta nueva", color = MaterialTheme.colorScheme.primary) }
+            ) { InstaText(text = stringResource(R.string.login_screen_button_register), color = MaterialTheme.colorScheme.primary) }
             Icon(
                 modifier = Modifier
                     .width(30.dp)
