32d129d2b564ab44be254afda4b882ff6f3174c9
rrufino71@gmail.com <rrufino71@gmail.com>
2026-05-26 14:59:34 -0300

    insta vista

--- app/src/main/java/com/example/instadev/view/auth/register/RegisterScreen.kt
+++ app/src/main/java/com/example/instadev/view/auth/register/RegisterScreen.kt
@@ -1,5 +1,6 @@
 package com.example.instadev.view.auth.register
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -16,10 +17,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.instadev.R
 import com.example.instadev.view.auth.core.components.InstaButton
@@ -30,6 +33,30 @@ import com.example.instadev.view.auth.core.components.InstaTextField
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen(registerViewModel: RegisterViewModel = viewModel()) {
+
+    //si en vez de por "by" pongo "=" cuando accedo a los valores debo poner ".value"
+    val uiState:RegisterUiState by registerViewModel.uiState.collectAsStateWithLifecycle()
+
+    val title: String
+    val subtitle: String
+    val label: String
+    val changeModeTitle: String
+
+    when(uiState.isPhoneMode){
+        true -> {
+            title=stringResource(R.string.register_screen_title_phone)
+            subtitle=stringResource(R.string.register_screen_subtitle_phone)
+            label=stringResource(R.string.register_screen_textfield_register_phone)
+            changeModeTitle=stringResource(R.string.register_screen_button_register_with_email)
+        }
+        false -> {
+            title=stringResource(R.string.register_screen_title_email)
+            subtitle=stringResource(R.string.register_screen_subtitle_email)
+            label=stringResource(R.string.register_screen_textfield_register_email)
+            changeModeTitle=stringResource(R.string.register_screen_button_register_with_phone)
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -54,23 +81,25 @@ fun RegisterScreen(registerViewModel: RegisterViewModel = viewModel()) {
                 .padding(horizontal = 16.dp)
                 .background(MaterialTheme.colorScheme.background)
         ) {
-            InstaText(
-                text = stringResource(R.string.register_screen_title),
-                style = MaterialTheme.typography.headlineLarge,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
+            AnimatedContent(title) { animatedTitle ->
+                InstaText(
+                    text = animatedTitle,
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+               )
+            }
             Spacer(Modifier.height(4.dp))
             InstaText(
-                text = stringResource(R.string.register_screen_subtitle),
+                text = subtitle,
                 style = MaterialTheme.typography.titleSmall,
                 color = MaterialTheme.colorScheme.onSurfaceVariant
             )
             Spacer(Modifier.height(16.dp))
             InstaTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = "",
-                label = stringResource(R.string.register_screen_textfield_register),
-                onValueChange = {}
+                value = uiState.value,
+                label = label,
+                onValueChange = {registerViewModel.onRegisterChanged(it)}
             )
             Spacer(Modifier.height(12.dp))
             InstaText(text = stringResource(R.string.register_screen_body))
@@ -78,13 +107,14 @@ fun RegisterScreen(registerViewModel: RegisterViewModel = viewModel()) {
             InstaButton(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = {},
+                enabled = uiState.isRegisterEnabled,
                 text = stringResource(R.string.register_screen_button_next)
             )
             Spacer(Modifier.height(4.dp))
             InstaButtonSecondary(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = {},
-                title = stringResource(R.string.register_screen_button_register_with_email),
+                onClick = {registerViewModel.onChangeMode()},
+                title = changeModeTitle,
                 titleColor = MaterialTheme.colorScheme.onPrimary,
                 border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
             )
--- app/src/main/java/com/example/instadev/view/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/instadev/view/auth/register/RegisterViewModel.kt
@@ -1,6 +1,41 @@
 package com.example.instadev.view.auth.register
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class RegisterViewModel: ViewModel() {
+class RegisterViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(RegisterUiState())
+    val uiState: StateFlow<RegisterUiState> = _uiState
+
+
+    fun onChangeMode() {
+        _uiState.update {
+            it.copy(isPhoneMode = !it.isPhoneMode,value = "")
+        }
+    }
+
+    fun onRegisterChanged(value:String) {
+        _uiState.update {state ->
+            val isEnabled = if(state.isPhoneMode) {
+                value.length >= 9
+            }else{
+                Patterns.EMAIL_ADDRESS.matcher(value).matches()
+            }
+            state.copy(isRegisterEnabled=isEnabled, value = value)
+        }
+    }
+
+
+
 }
+
+data class RegisterUiState(
+    val value: String = "",
+    val isPhoneMode: Boolean = true,
+    val isRegisterEnabled: Boolean = false
+)
+
+//ver sealed class y enum class
